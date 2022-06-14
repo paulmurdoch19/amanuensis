ac73f9b40d7f38f87e4061dfcffa81223c9504f0
Nicholas Ferraz <ferraz@bsd.uchicago.edu>
2022-06-14 11:21:41 -0500

    update_project_state() now checks for invalid parameters and updates the request.request_has_state instead of request.state

--- amanuensis/resources/admin/admin_state.py
+++ amanuensis/resources/admin/admin_state.py
@@ -4,7 +4,11 @@ from cdislogging import get_logger
 
 from amanuensis.resources import userdatamodel as udm
 from amanuensis.config import config
-from amanuensis.schema import StateSchema, RequestSchema, ConsortiumDataContributorSchema
+from amanuensis.schema import (
+    StateSchema,
+    RequestSchema,
+    ConsortiumDataContributorSchema,
+)
 from amanuensis.errors import NotFound
 
 
@@ -15,46 +19,51 @@ __all__ = [
     "get_all_states",
     "update_project_state",
     "create_consortium",
-    "get_by_code"
+    "get_by_code",
 ]
 
 
 def create_state(name, code):
-    with flask.current_app.db.session as session:  
-        state_schema = StateSchema()  
+    with flask.current_app.db.session as session:
+        state_schema = StateSchema()
         state = udm.create_state(session, name, code)
         state_schema.dump(state)
         return state
 
 
 def get_all_states():
-    with flask.current_app.db.session as session:    
+    with flask.current_app.db.session as session:
         return udm.get_all_states(session)
 
+
 def get_by_code(code):
-    with flask.current_app.db.session as session:    
+    with flask.current_app.db.session as session:
         return udm.get_state_by_code(session, code)
 
 
 def update_project_state(project_id, state_id):
-    with flask.current_app.db.session as session: 
+    with flask.current_app.db.session as session:
         requests = udm.get_requests_by_project_id(session, project_id)
         if not requests:
-            raise NotFound("There are no requests associated to this project or there is no project. id: {}".format(project_id))
+            raise NotFound(
+                "There are no requests associated to this project or there is no project. id: {}".format(
+                    project_id
+                )
+            )
 
         state = udm.get_state_by_id(session, state_id)
         if not state:
             raise NotFound("The state with id {} has not been found".format(state_id))
 
-        request_schema = RequestSchema(many=True)  
+        request_schema = RequestSchema(many=True)
         requests = udm.update_project_state(session, requests, state)
-        request_schema.dump(requests) 
+        request_schema.dump(requests)
         return requests
 
 
 def create_consortium(name, code):
-    with flask.current_app.db.session as session:  
-        consortium_schema = ConsortiumDataContributorSchema()  
+    with flask.current_app.db.session as session:
+        consortium_schema = ConsortiumDataContributorSchema()
         consortium = udm.create_consortium(session, name, code)
         consortium_schema.dump(consortium)
         return consortium
--- amanuensis/resources/userdatamodel/userdatamodel_state.py
+++ amanuensis/resources/userdatamodel/userdatamodel_state.py
@@ -1,10 +1,10 @@
+from logging import getLogger
 from sqlalchemy import func
+from amanuensis.resources.aws import boto_manager
 
+from amanuensis.resources.userdatamodel.userdatamodel_message import send_message
 from amanuensis.errors import NotFound, UserError
-from amanuensis.models import (
-    State,
-    ConsortiumDataContributor
-)
+from amanuensis.models import State, ConsortiumDataContributor, RequestState
 
 __all__ = [
     "create_state",
@@ -15,6 +15,8 @@ __all__ = [
     "get_state_by_code",
 ]
 
+logger = getLogger(__name__)
+
 
 def create_consortium(current_session, name, code):
     """
@@ -24,7 +26,7 @@ def create_consortium(current_session, name, code):
 
     current_session.add(new_consortium)
     current_session.flush()
-    
+
     return new_consortium
 
 
@@ -37,7 +39,7 @@ def create_state(current_session, name, code):
     current_session.add(new_state)
     # current_session.commit()
     current_session.flush()
-    
+
     return new_state
 
 
@@ -53,12 +55,52 @@ def get_all_states(current_session):
     return current_session.query(State).all()
 
 
+def notify_user_of_state_change(request, state, current_session):
+    """
+    Notify the user of a state change
+    """
+    email_subject = "State Change"
+    email_body = "Your request has been moved to the state {}".format(state.name)
+    # send_message(current_session, logged_user_id, request_id, subject, body, receivers, emails):
+    # TODO change hard-coded values to be configurable
+    send_message(
+        current_session,
+        7,
+        request.id,
+        email_subject,
+        email_body,
+        ["ferraz@uchicago.edu"],
+        ["caixadonick@gmail.com"],
+    )
+
+
 def update_project_state(current_session, requests, state):
     for request in requests:
-        request.states.append(state)
+        """
+        Comparing request.state with state won't work because the request object doesn't know about the
+        RequestState object. To do this you need to either:
+        1. Query the database for the RequestState object to compare
+        2. Go back to the old way of using request.states.append(state) but in a way that allows you
+        to have a new create_date--for some reason it seems to be using the create_date of
+        the state, not the new one from the RequestState object.
+        """
+        if (
+            request.request_has_state[-1].state_id == 2
+            or request.request_has_state[-1].state_id == 4
+        ):
+            raise UserError(
+                "Cannot change state of request {} from {} because it's a final state".format(
+                    request.id, state.code
+                )
+            )
+        elif request.request_has_state[-1].state_id == state.id:
+            raise UserError(
+                "Request {} is already in state {}".format(request.id, state.code)
+            )
+        else:
+            request.request_has_state.append(
+                RequestState(state_id=state.id, request_id=request.id)
+            )
 
     current_session.flush()
     return requests
-
-
-
