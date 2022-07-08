ad735f0ef8622b3488cd3d4e397f8e4a06db0add
Nicholas Ferraz <ferraz@bsd.uchicago.edu>
2022-07-08 14:16:14 -0500

    Using config file for consortia statuses

--- amanuensis/resources/userdatamodel/userdatamodel_state.py
+++ amanuensis/resources/userdatamodel/userdatamodel_state.py
@@ -1,7 +1,5 @@
 from logging import getLogger
-from sqlalchemy import func
-from amanuensis.resources.aws import boto_manager
-from amanuensis.auth.auth import current_user
+from sqlalchemy import func, desc
 from amanuensis.resources.message import send_message
 from amanuensis.resources.userdatamodel.userdatamodel_project import get_project_by_id
 from amanuensis.errors import NotFound, UserError
@@ -10,7 +8,7 @@ from amanuensis.models import (
     ConsortiumDataContributor,
     RequestState,
     Receiver,
-    Project
+    Project,
 )
 
 __all__ = [
@@ -24,6 +22,7 @@ __all__ = [
 
 logger = getLogger(__name__)
 
+
 def create_consortium(current_session, name, code):
     """
     Creates a consortium
@@ -32,7 +31,7 @@ def create_consortium(current_session, name, code):
 
     current_session.add(new_consortium)
     current_session.flush()
-    
+
     return new_consortium
 
 
@@ -45,7 +44,7 @@ def create_state(current_session, name, code):
     current_session.add(new_state)
     # current_session.commit()
     current_session.flush()
-    
+
     return new_state
 
 
@@ -60,32 +59,51 @@ def get_state_by_code(current_session, code):
 def get_all_states(current_session):
     return current_session.query(State).all()
 
-def notify_user_data_delivered(request, current_session):
+
+def notify_user_project_status_update(current_user_id, request, current_session):
     """
-    Notify the users when project state changes to DATA_DELIVERED.
+    Notify the users when project state changes.
     """
     project = get_project_by_id(current_session, request.project_id)
     email_subject = f"Project {project.name}: Data Delivered"
     email_body = f"The project f{project.name} data was delivered."
 
-    return send_message(current_user.id, request.id, email_subject, email_body)
+    return send_message(current_user_id, request.id, email_subject, email_body)
 
-def update_project_state(current_session, requests, state):
-    '''
+
+def update_project_state(
+    current_user_id, current_session, requests, state, consortium_statuses
+):
+    """
     Updates the state for a project, including all requests in the project. Notifies users when the state changes to DATA_DELIVERED.
-    '''
-
+    """
+    updated = False
     for request in requests:
-        if request.request_has_state[-1].state_id == 2 or request.request_has_state[-1].state_id == 4:
-            raise UserError("Cannot change state of request {} from {} because it's a final state"\
-                .format(request.id, state.code))
-        elif request.request_has_state[-1].state_id == state.id:
-            raise UserError("Request {} is already in state {}".format(request.id, state.code))
+        state_code = request.request_has_state[-1].state.code
+
+        if state_code == state.code:
+            logger.info(
+                "Request {} is already in state {}. No need to change.".format(
+                    request.id, state.code
+                )
+            )
+        elif state_code in consortium_statuses["Final"]:
+            raise UserError(
+                "Cannot change state of request {} from {} because it's a final state".format(
+                    request.id, state.code
+                )
+            )
         else:
-            request.request_has_state.append(RequestState(state_id=state.id, request_id=request.id))
-            
-    if state.code == "DATA_DELIVERED":
-        notify_user_data_delivered(request, current_session)
+            request.request_has_state.append(
+                RequestState(state_id=state.id, request_id=request.id)
+            )
+            updated = True
+
+    if state.code in consortium_statuses["Notify"] and updated:
+        message = notify_user_project_status_update(
+            current_user_id, request, current_session
+        )
+        logger.info(message)
 
     current_session.flush()
     return requests
