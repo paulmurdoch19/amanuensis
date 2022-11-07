6367becaa029303d854f6178c48c014f8298c358
Nicholas Ferraz <9021595+nikoferraz@users.noreply.github.com>
2022-11-07 14:05:18 -0600

    Checking if user is already present in table

--- amanuensis/resources/admin/admin_associated_user.py
+++ amanuensis/resources/admin/admin_associated_user.py
@@ -7,7 +7,6 @@ from amanuensis.config import config
 from amanuensis.schema import StateSchema, RequestSchema, ConsortiumDataContributorSchema, AssociatedUserSchema
 from amanuensis.errors import NotFound
 
-
 logger = get_logger(__name__)
 
 __all__ = [
@@ -25,10 +24,17 @@ def add_associated_users(users):
     with flask.current_app.db.session as session:
         associated_user_schema = AssociatedUserSchema(many=True)
         ret = []
+        user_emails = [user["email"] for user in users]
+        users_in_table = udm.get_associated_users(session, user_emails)
         for user in users:
-            ret.append(udm.add_associated_user(session, user["project_id"], user["email"] if "email" in user else None, user["id"] if "id" in user else None))
-        associated_user_schema.dump(ret)
+            if user not in users_in_table:
+                ret.append(
+                    udm.add_associated_user(
+                        session, 
+                        user["project_id"], 
+                        user["email"] if "email" in user else None,
+                        user["id"] if "id" in user else None
+                    )
+                )
+                associated_user_schema.dump(ret)
         return ret
-
-
-
