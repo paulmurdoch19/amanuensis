ffda19cb8a88c84ad02780dcf55884fca57690ff
Nicholas Ferraz <9021595+nikoferraz@users.noreply.github.com>
2022-12-12 14:21:37 -0600

    Added request states

---
+++ migrations/versions/fa0c3fdf48ea_add_request_states.py
@@ -0,0 +1,64 @@
+"""Add request states
+
+Revision ID: fa0c3fdf48ea
+Revises: f7ef7b53a36e
+Create Date: 2022-12-12 19:23:41.792566
+
+"""
+import logging
+
+from alembic import op
+from sqlalchemy.orm import Session
+from userportaldatamodel.models import State
+
+# revision identifiers, used by Alembic.
+revision = "fa0c3fdf48ea"
+down_revision = "f7ef7b53a36e"
+branch_labels = None
+depends_on = None
+
+logger = logging.getLogger("amanuensis.alembic")
+
+
+def upgrade() -> None:
+
+    conn = op.get_bind()
+    session = Session(bind=conn)
+    states = []
+
+    states.append(State(name="Draft", code="DRAFT"))
+
+    states.append(State(name="Submitted", code="SUBMITTED"))
+
+    states.append(State(name="Revision", code="REVISION"))
+
+    states.append(State(name="Approved with Feedback", code="APPROVED_WITH_FEEDBACK"))
+
+    states.append(
+        State(name="Request Criteria Finalized", code="REQUEST_CRITERIA_FINALIZED")
+    )
+
+    states.append(State(name="Withdrawal", code="WITHDRAWAL"))
+
+    states.append(
+        State(
+            name="Agreements Negotiation",
+            code="AGREEMENTS_NEGOTIATION",
+        )
+    )
+
+    states.append(State(name="Agreements Executed", code="AGREEMENTS_EXECUTED"))
+
+    states.append(State(name="Data Available", code="DATA_AVAILABLE"))
+
+    states.append(State(name="Data Downloaded", code="DATA_DOWNLOADED"))
+
+    states.append(State(name="Published", code="PUBLISHED"))
+
+    session.add_all(states)
+    session.commit()
+
+
+def downgrade() -> None:
+    pass
+
