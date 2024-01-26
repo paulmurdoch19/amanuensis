c441534453edc8e9c8d613d86d55c0fb0508be21
paulmurdoch19 <paulmurdoch19@gmail.com>
2024-01-26 11:05:42 -0800

    remove old tests

--- tests_endpoints/endpoints/test_misc.py
+++
@@ -1,7 +0,0 @@
-def test_status(client):
-    response = client.get("/_status")
-    assert response.status_code == 200
-
-def test_version(client):
-    response = client.get("/_version")
-    assert response.status_code == 200
